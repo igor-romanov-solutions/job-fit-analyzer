b99a46f0ab64b395445be1224a0769ac61642bdf
Igor Romanov <igor.romanov.vc@gmail.com>
2026-04-17 18:41:32 +0200

    ci: fail build when tests fail

---
+++ src/test/java/me/romanov/jobfitanalyzer/FailTest.java
@@ -0,0 +1,12 @@
+package me.romanov.jobfitanalyzer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class FailTest {
+    @Test
+    void fail() {
+        assertNotNull(null);
+    }
+}
